43813d167eacf53646610a2d5f41baa028b5138a
Ronak Shah <ronak.shah@dalaltimes.com>
2015-09-16 12:49:30 +0530

    HTML Version of new Dalaltimes.com

---
+++ Sedna - A Free HTML5_CSS3 website by Peter Finlan_files/webslidemenu.js
@@ -0,0 +1,48 @@
+/*var _0xaa90=["\x2E\x6F\x76\x65\x72\x6C\x61\x70\x62\x6C\x61\x63\x6B\x62\x67\x2C\x20\x2E\x73\x6C\x69\x64\x65\x4C\x65\x66\x74","\x2E\x77\x73\x6D\x65\x6E\x75\x63\x6F\x6E\x74\x65\x6E\x74","\x6D\x65\x6E\x75\x6F\x70\x65\x6E","\x61\x64\x64\x43\x6C\x61\x73\x73","\x6D\x65\x6E\x75\x63\x6C\x6F\x73\x65","\x72\x65\x6D\x6F\x76\x65\x43\x6C\x61\x73\x73","\x68\x61\x73\x43\x6C\x61\x73\x73","\x63\x6C\x69\x63\x6B","\x23\x6E\x61\x76\x54\x6F\x67\x67\x6C\x65","\x6D\x72\x67\x69\x6E\x6C\x65\x66\x74","\x74\x6F\x67\x67\x6C\x65\x43\x6C\x61\x73\x73","\x2E\x77\x73\x6D\x65\x6E\x75\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x6F\x6E","\x23\x6E\x61\x76\x54\x6F\x67\x67\x6C\x65\x2C\x2E\x6F\x76\x65\x72\x6C\x61\x70\x62\x6C\x61\x63\x6B\x62\x67","\x3C\x73\x70\x61\x6E\x20\x63\x6C\x61\x73\x73\x3D\x22\x77\x73\x6D\x65\x6E\x75\x2D\x63\x6C\x69\x63\x6B\x22\x3E\x3C\x69\x20\x63\x6C\x61\x73\x73\x3D\x22\x77\x73\x6D\x65\x6E\x75\x2D\x61\x72\x72\x6F\x77\x20\x66\x61\x20\x66\x61\x2D\x61\x6E\x67\x6C\x65\x2D\x64\x6F\x77\x6E\x22\x3E\x3C\x2F\x69\x3E\x3C\x2F\x73\x70\x61\x6E\x3E","\x70\x72\x65\x70\x65\x6E\x64","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x73\x75\x62\x6D\x65\x6E\x75\x2C\x20\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x73\x75\x62\x6D\x65\x6E\x75\x2D\x73\x75\x62\x2C\x20\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x73\x75\x62\x6D\x65\x6E\x75\x2D\x73\x75\x62\x2D\x73\x75\x62","\x68\x61\x73","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x6C\x69\x73\x74\x20\x6C\x69","\x2E\x6D\x65\x67\x61\x6D\x65\x6E\x75","\x73\x6C\x6F\x77","\x73\x6C\x69\x64\x65\x54\x6F\x67\x67\x6C\x65","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x6C\x69\x73\x74","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x6D\x6F\x62\x69\x6C\x65","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x73\x75\x62\x6D\x65\x6E\x75","\x73\x69\x62\x6C\x69\x6E\x67\x73","\x77\x73\x6D\x65\x6E\x75\x2D\x72\x6F\x74\x61\x74\x65","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x61\x72\x72\x6F\x77","\x63\x68\x69\x6C\x64\x72\x65\x6E","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x73\x75\x62\x6D\x65\x6E\x75\x2D\x73\x75\x62","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x73\x75\x62\x6D\x65\x6E\x75\x2D\x73\x75\x62\x2D\x73\x75\x62","\x2E\x77\x73\x6D\x65\x6E\x75\x2D\x63\x6C\x69\x63\x6B"];$(function(){var _0x6712x1=$(_0xaa90[0]);var _0x6712x2=$(_0xaa90[1]);var _0x6712x3=function(){$(_0x6712x1)[_0xaa90[5]](_0xaa90[4])[_0xaa90[3]](_0xaa90[2])};var _0x6712x4=function(){$(_0x6712x1)[_0xaa90[5]](_0xaa90[2])[_0xaa90[3]](_0xaa90[4])};$(_0xaa90[8])[_0xaa90[7]](function(){if(_0x6712x2[_0xaa90[6]](_0xaa90[2])){$(_0x6712x4)}else {$(_0x6712x3)}});_0x6712x2[_0xaa90[7]](function(){if(_0x6712x2[_0xaa90[6]](_0xaa90[2])){$(_0x6712x4)}});$(_0xaa90[13])[_0xaa90[12]](_0xaa90[7],function(){$(_0xaa90[11])[_0xaa90[10]](_0xaa90[9])});$(_0xaa90[18])[_0xaa90[17]](_0xaa90[16])[_0xaa90[15]](_0xaa90[14]);$(_0xaa90[18])[_0xaa90[17]](_0xaa90[19])[_0xaa90[15]](_0xaa90[14]);$(_0xaa90[23])[_0xaa90[7]](function(){$(_0xaa90[22])[_0xaa90[21]](_0xaa90[20])});$(_0xaa90[31])[_0xaa90[7]](function(){$(this)[_0xaa90[25]](_0xaa90[24])[_0xaa90[21]](_0xaa90[20]);$(this)[_0xaa90[28]](_0xaa90[27])[_0xaa90[10]](_0xaa90[26]);$(this)[_0xaa90[25]](_0xaa90[29])[_0xaa90[21]](_0xaa90[20]);$(this)[_0xaa90[25]](_0xaa90[30])[_0xaa90[21]](_0xaa90[20]);$(this)[_0xaa90[25]](_0xaa90[19])[_0xaa90[21]](_0xaa90[20]);});});*/
+/** @type {Array} */
+var _0xaa90 = [".overlapblackbg, .slideLeft", ".wsmenucontent", "menuopen", "addClass", "menuclose", "removeClass", "hasClass", "click", "#navToggle", "mrginleft", "toggleClass", ".wsmenucontainer", "on", "#navToggle,.overlapblackbg", '<span class="wsmenu-click"><i class="wsmenu-arrow fa fa-angle-down"></i></span>', "prepend", ".wsmenu-submenu, .wsmenu-submenu-sub, .wsmenu-submenu-sub-sub", "has", ".wsmenu-list li", ".megamenu", "slow", "slideToggle", ".wsmenu-list", ".wsmenu-mobile", ".wsmenu-submenu",
+"siblings", "wsmenu-rotate", ".wsmenu-arrow", "children", ".wsmenu-submenu-sub", ".wsmenu-submenu-sub-sub", ".wsmenu-click"];
+$(function() {
+  var headings = $(".overlapblackbg, .slideLeft");
+  var emptyJ = $(".wsmenucontent");
+  /**
+   * @return {undefined}
+   */
+  var backdrop = function() {
+    alert("222");
+    $(headings)["removeClass"]("menuclose")["addClass"]("menuopen");
+  };
+  /**
+   * @return {undefined}
+   */
+  var _element = function() {
+    $(headings)["removeClass"]("menuopen")["addClass"]("menuclose");
+  };
+  $("#navToggle")["click"](function() {
+    if (emptyJ["hasClass"]("menuopen")) {
+      $(_element);
+    } else {
+      $(backdrop);
+    }
+  });
+  emptyJ["click"](function() {
+    if (emptyJ["hasClass"]("menuopen")) {
+      $(_element);
+    }
+  });
+  $("#navToggle,.overlapblackbg")["on"]("click", function() {
+    $(".wsmenucontainer")["toggleClass"]("mrginleft");
+  });
+  $(".wsmenu-list li")["has"](".wsmenu-submenu, .wsmenu-submenu-sub, .wsmenu-submenu-sub-sub")["prepend"]('<span class="wsmenu-click"><i class="wsmenu-arrow fa fa-angle-down"></i></span>');
+  $(".wsmenu-list li")["has"](".megamenu")["prepend"]('<span class="wsmenu-click"><i class="wsmenu-arrow fa fa-angle-down"></i></span>');
+  $(".wsmenu-mobile")["click"](function() {
+    $(".wsmenu-list")["slideToggle"]("slow");
+  });
+  $(".wsmenu-click")["click"](function() {
+    $(this)["siblings"](".wsmenu-submenu")["slideToggle"]("slow");
+    $(this)["children"](".wsmenu-arrow")["toggleClass"]("wsmenu-rotate");
+    $(this)["siblings"](".wsmenu-submenu-sub")["slideToggle"]("slow");
+    $(this)["siblings"](".wsmenu-submenu-sub-sub")["slideToggle"]("slow");
+    $(this)["siblings"](".megamenu")["slideToggle"]("slow");
+  });
+});
