3e8e00cf4ee119da7291aac009172cb3ca54851d
Ronak Shah <ronak.shah@dalaltimes.com>
2015-11-05 15:19:49 +0530

    made changes in the article & homepage

--- class/class.functions.php
+++ class/class.functions.php
@@ -9,7 +9,7 @@ class CommonFunctions {
 		$string = strtolower(str_replace($replace, "", trim($string)));
 		$string = str_replace("  ", " ", trim($string));
 		$string = str_replace(" ", "-", trim($string));*/
-		$string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+		$string = str_replace(' ', '-', strtolower($string)); // Replaces all spaces with hyphens.
 		$string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 		return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
 	}
